cd17cb736a7e658d323346223f58cddcb08b7e1c
David Halonen <dhalonen@me.com>
2021-04-16 21:45:04 -0400

    Addition of test & playground.

--- saunaPiPhone/urlPlayground.playground/Contents.swift
+++ saunaPiPhone/urlPlayground.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-var str = "Hello, playground"
+let url = URL(string: "http://saunapi.harmon/data.json")!
+
+let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+    guard let data = data else { return }
+    print(String(data: data, encoding: .utf8)!)
+}
+
+task.resume()
